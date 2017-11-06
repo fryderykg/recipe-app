96e563936dd1576fa587783bca5ba4621a91bce3
FryderykG <eszkimos@gmail.com>
2017-11-06 21:32:53 +0100

    add recipe model, and display test recipes in recipe-list

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,10 +8,15 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[];
 
   constructor() { }
 
   ngOnInit() {
+    this.recipes = [
+      new Recipe('Test recipe', 'This is a test', 'http://www.artnfood.co.uk/wp-content/uploads/2015/03/paella.jpg'),
+      new Recipe('Test recipe 2', 'This is a test 222', 'http://maxpixel.freegreatpicture.com/static/photo/640/Red-Strawberry-Delicious-Sweet-82529.jpg')
+    ];
   }
 
 }
