6e018098b24e70b71993fe120d888a32cd0179eb
FryderykG <eszkimos@gmail.com>
2017-11-07 20:51:11 +0100

    add ingredient model, and template to shopping-list and shopping-list-edit

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,10 +8,16 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[];
 
   constructor() { }
 
   ngOnInit() {
+    this.ingredients = [
+      new Ingredient('apple', 10),
+      new Ingredient('potato', 15),
+      new Ingredient('banana', 25)
+    ];
   }
 
 }
